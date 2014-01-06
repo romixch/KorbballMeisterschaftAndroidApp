1236a382429d4d7045de5259d1468be6b0aabfc8
Roman Schaller <roman.schaller@gmail.com>
2014-01-07 00:06:04 +0100

    add feature poll

---
+++ src/ch/romix/korbball/meisterschaft/GetGroupsTask.java
@@ -0,0 +1,98 @@
+package ch.romix.korbball.meisterschaft;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.BaseAdapter;
+
+class GetGroupsTask extends AsyncTask<Void, Void, List<Map<String, String>>> {
+	private final List<Map<String, String>> groupsByGroupId;
+	private final BaseAdapter adapter;
+	private final Context context;
+	private Exception exception;
+
+	public GetGroupsTask(List<Map<String, String>> groupsByGroupId, BaseAdapter adapter, Context context) {
+		this.groupsByGroupId = groupsByGroupId;
+		this.adapter = adapter;
+		this.context = context;
+	}
+
+	@Override
+	protected List<Map<String, String>> doInBackground(Void... params) {
+		final List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(UrlConsts.GROUPS);
+		HttpResponse response;
+		try {
+			response = httpclient.execute(httppost);
+			String jsonResult = StreamTools.inputStreamToString(response.getEntity().getContent()).toString();
+			JSONObject object = new JSONObject(jsonResult);
+			@SuppressWarnings("rawtypes")
+			Iterator it = object.keys();
+			while (it.hasNext()) {
+				String groupId = it.next().toString();
+				String groupName = object.get(groupId).toString();
+				HashMap<String, String> map = new HashMap<String, String>();
+				map.put(Start.GROUP_ID, groupId);
+				map.put(Start.GROUP_NAME, groupName);
+				result.add(map);
+			}
+			Collections.sort(result, new Comparator<Map<String, String>>() {
+				@Override
+				public int compare(Map<String, String> lhs, Map<String, String> rhs) {
+					return lhs.get(Start.GROUP_NAME).compareTo(rhs.get(Start.GROUP_NAME));
+				}
+			});
+		} catch (ClientProtocolException e) {
+			setException(e);
+		} catch (IOException e) {
+			setException(e);
+		} catch (JSONException e) {
+			setException(e);
+		}
+		return result;
+	}
+
+	private void setException(Exception exception) {
+		this.exception = exception;
+	}
+
+	@Override
+	protected void onPostExecute(List<Map<String, String>> result) {
+		groupsByGroupId.clear();
+		if (hasException()) {
+			Builder builder = new AlertDialog.Builder(context);
+			builder.setMessage(R.string.noConnection);
+			builder.setCancelable(true);
+			builder.create().show();
+		} else if (result != null) {
+			for (Map<String, String> map : result) {
+				groupsByGroupId.add(map);
+			}
+			adapter.notifyDataSetChanged();
+		}
+		super.onPostExecute(result);
+	}
+
+	private boolean hasException() {
+		return exception != null;
+	}
+}
--- src/ch/romix/korbball/meisterschaft/Start.java
+++ src/ch/romix/korbball/meisterschaft/Start.java
@@ -1,30 +1,18 @@
 package ch.romix.korbball.meisterschaft;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,9 +23,11 @@ import android.widget.SimpleAdapter;
 
 public class Start extends Activity {
 
-	private static final int NO_DATA_MSG = 0;
-	private static final String GROUP_NAME = "name";
-	private static final String GROUP_ID = "id";
+	private static final long ONE_MONTH_IN_MILLIS = 30L * 24 * 60 * 60 * 1000;
+	private static final long ONE_DAY_IN_MILLIS = 24L * 60 * 60 * 1000;
+	static final String GROUP_NAME = "name";
+	static final String GROUP_ID = "id";
+	private static final String LAST_POPUP_DATE = "LAST_POPUP_DATE";
 	private List<Map<String, String>> groupsByGroupId;
 	private SimpleAdapter simpleAdapter;
 
@@ -61,19 +51,47 @@ public class Start extends Activity {
 				startActivity(myIntent);
 			}
 		});
-		Handler exceptionHandler = new Handler() {
+		new GetGroupsTask(groupsByGroupId, simpleAdapter, this).execute();
+		askToFillFeedbackForm();
+	}
+
+	private void askToFillFeedbackForm() {
+		if (shouldShowPopupNow()) {
+			showPopup();
+		}
+	}
+
+	private boolean shouldShowPopupNow() {
+		SharedPreferences preferences = getPreferences(Activity.MODE_PRIVATE);
+		final long nextPopupInMillis = preferences.getLong(LAST_POPUP_DATE, System.currentTimeMillis());
+		return System.currentTimeMillis() > nextPopupInMillis;
+	}
+
+	private void showPopup() {
+		Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Was darf es sein?");
+		builder.setMessage("Liebe(r) Korbballer(in). Damit ich beim nächsten Update deine liebste Funktion ausliefern kann, bitte ich dich, mir kurz ein paar Ideen zu bewerten.");
+		builder.setPositiveButton("Jetzt bewerten", new OnClickListener() {
+
 			@Override
-			public void handleMessage(Message msg) {
-				switch (msg.what) {
-				case NO_DATA_MSG:
-					Builder builder = new AlertDialog.Builder(Start.this);
-					builder.setMessage(R.string.noConnection);
-					builder.setCancelable(true);
-					builder.create().show();
-				}
+			public void onClick(DialogInterface dialog, int which) {
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://romixch.typeform.com/to/r3ELGk"));
+				startActivity(browserIntent);
+				setNextPopupTime(ONE_MONTH_IN_MILLIS);
 			}
-		};
-		new GetGroupsTask(exceptionHandler).execute();
+		});
+		builder.setNegativeButton("Später erinnern", new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				setNextPopupTime(ONE_DAY_IN_MILLIS);
+			}
+		});
+		builder.create();
+		builder.show();
+	}
+
+	private void setNextPopupTime(long showAgainInMillis) {
+		getPreferences(MODE_PRIVATE).edit().putLong(LAST_POPUP_DATE, System.currentTimeMillis() + showAgainInMillis).commit();
 	}
 
 	@Override
@@ -90,57 +108,5 @@ public class Start extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	private class GetGroupsTask extends AsyncTask<Void, Void, List<Map<String, String>>> {
-		private final Handler exceptionHandler;
-
-		public GetGroupsTask(Handler exceptionHandler) {
-			this.exceptionHandler = exceptionHandler;
-		}
-
-		@Override
-		protected List<Map<String, String>> doInBackground(Void... params) {
-			groupsByGroupId.clear();
-			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost(UrlConsts.GROUPS);
-			HttpResponse response;
-			try {
-				response = httpclient.execute(httppost);
-				String jsonResult = StreamTools.inputStreamToString(response.getEntity().getContent()).toString();
-				JSONObject object = new JSONObject(jsonResult);
-				@SuppressWarnings("rawtypes")
-				Iterator it = object.keys();
-				while (it.hasNext()) {
-					String groupId = it.next().toString();
-					String groupName = object.get(groupId).toString();
-					HashMap<String, String> map = new HashMap<String, String>();
-					map.put(GROUP_ID, groupId);
-					map.put(GROUP_NAME, groupName);
-					groupsByGroupId.add(map);
-					Collections.sort(groupsByGroupId, new Comparator<Map<String, String>>() {
-						@Override
-						public int compare(Map<String, String> lhs, Map<String, String> rhs) {
-							return lhs.get(GROUP_NAME).compareTo(rhs.get(GROUP_NAME));
-						}
-					});
-				}
-			} catch (ClientProtocolException e) {
-			} catch (IOException e) {
-			} catch (JSONException e) {
-				Message msg = new Message();
-				msg.what = NO_DATA_MSG;
-				exceptionHandler.sendMessage(msg);
-			}
-			return groupsByGroupId;
-		}
-
-		@Override
-		protected void onPostExecute(List<Map<String, String>> result) {
-			if (result != null) {
-				simpleAdapter.notifyDataSetChanged();
-			}
-			super.onPostExecute(result);
-		}
 	}
 }
